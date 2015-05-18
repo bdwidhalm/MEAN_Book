f04afb0f397c63c780155ab4fef6e4702e2139a5
Brian Widhalm <bdwidhalm@gmail.com>
2015-05-18 07:44:02 -0600

    HRapp

--- HRapp/index.js
+++ HRapp/index.js
@@ -1,4 +1,5 @@
 var http = require('http');
+var employeeService = require('./lib/employees');
 
 http.createServer(function (req, res) {
     // A parsed url to work with in case there are parameters
@@ -17,16 +18,35 @@ http.createServer(function (req, res) {
     
     if (_url = /^\/employees$/i.exec(req.url)) {
         // return a list of employees
-        res.writeHead(200);
-        return res.end('employee list');
+        employeeService.getEmployees(function (error, data) {
+            if (error) {
+                // send a 500 error
+            }
+            // send the data with a 200 status code
+            // res.writeHead(200);
+            // return res.end('employee list');
+        });
     } else if (_url = /^\/employees\/(\d+)$/i.exec(req.url)) {
         // find the employee by the id in the route
-        res.writeHead(200);
-        return res.end('a single employee');
+        employeeService.getEmployee(_url[1], function (error, data) {
+            if (error) {
+                // send a 500 error
+            }
+            
+            if (!data) {
+                // send a 404 error
+            }
+            
+            // send the data with a 200 status code
+            // res.writeHead(200);
+            // return res.end('a single employee');
+        });
     } else {
-        // try to send the static file
-        res.writeHead(200);
-        res.end('static file maybe');
+        // try to send the static file if it exists,
+        // if not, send a 404
+        
+        // res.writeHead(200);
+        // res.end('static file maybe');
     }
     
 }).listen(1337, '127.0.0.1');
