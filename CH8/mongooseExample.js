ca01799c7ee9c81e1a6e6a379be805b3e0e03081
Brian Widhalm <bdwidhalm@gmail.com>
2015-05-27 18:25:32 -0600

    Chapter 8

---
+++ CH8/mongooseExample.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var TeamSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  }
+});
+
+var EmployeeSchema = new Schema({
+  name: {
+    
+  }
+})
