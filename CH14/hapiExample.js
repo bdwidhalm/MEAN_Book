181b576892a7464ca2e5ba6b05043ef14e0fbeaa
Brian Widhalm <bdwidhalm@gmail.com>
2015-06-03 13:22:26 -0600

    Chapter 14

---
+++ CH14/hapiExample.js
@@ -0,0 +1,84 @@
+var Hapi = require('hapi');
+var joi = require('joi');
+var server = new Hapi.Server('localhost', 3000);
+
+server.route({
+  method: 'GET',
+  path: '/users',
+  config: {
+    handler: function (request, reply) {
+      setTimeout(function () {
+        result.users = [{
+          first: 'Abraham',
+          last: 'Lincoln'
+        }, {
+          first: 'Andrew',
+          last: 'Johnson'
+        }, {
+          first: 'Ulysses',
+          last: 'Grant'
+        }];
+        result.time = Date.now();
+
+        return reply(result).header('X-Special-Header', 'MEAN Stack');
+      }, 1000);
+    }
+  },
+  cache: {
+    expiresIn: 30000
+  },
+  validate: {
+    query: joi.object().keys({
+      page: joi.number().integer().min(1).max(10),
+      number: joi.number().integer().min(1).max(5)
+    })
+  }
+});
+
+
+var plug = {
+  register: function (plugin, options, next) {
+    plugin.route({
+      method: 'GET',
+      path: options.prefix + '/view',
+      config: {
+        handler: function (request, reply) {
+          request.server.inject({
+            url: '/users'
+          }, function (res) {
+            var users = res.result.users;
+            var view = '<DOCTYPE html><html lang="en"><body><h1>User Output</h1><table>';
+
+            for (var i = 0; i < users.length; i++) {
+              var user = users[i];
+              view += '<tr><td>' + user.first + '</td><td>' + user.last + '</td></tr>';
+            }
+
+            view += '</table></body></html>';
+            reply(view);
+          });
+        }
+      }
+    });
+
+    next();
+  }
+};
+plug.register.attributes = {
+  name: 'viewer',
+  version: '1.0.0'
+};
+
+
+server.pack.register({
+  plugin: plug,
+  options: {
+    prefix: '/users'
+  }
+}, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    server.start();
+  }
+});
