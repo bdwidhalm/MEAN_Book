57b5f7ec0178873edee142ae2f511046337e10a6
Brian Widhalm <bdwidhalm@gmail.com>
2015-06-08 07:26:25 -0600

    Ch 20

--- HRapp/index.js
+++ HRapp/index.js
@@ -12,8 +12,8 @@ var teams = require('./routes/teams');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
