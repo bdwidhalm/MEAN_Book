a116c463e49a4ad37eef53a1c8f2c37f568d046f
Brian Widhalm <bdwidhalm@gmail.com>
2015-06-08 21:45:24 -0600

    Chapter 20

--- HRapp/public/js/app.js
+++ HRapp/public/js/app.js
@@ -177,3 +177,22 @@ app.controller('EmployeeCtrl', ['$scope', '$routeParams', 'EmployeeService', 'Te
     lines.splice(index, 1);
   }
 }]);
+
+app.controller('TeamsCtrl', ['$scope', 'TeamService', function($scope, service) {
+  service.query(function(data) {
+    $scope.teams = data;
+  }, _handleError);
+}]);
+
+app.controller('TeamCtrl', ['$scope', '$routeParams', 'TeamService', function($scope, $routeParams, service) {
+  service.get({
+    teamId: $routeParams.teamId
+  }, function(data, headers) {
+    $scope.team = data;
+  }, _handleError);
+}]);
+
+function _handleError(response) {
+  // TODO: Do something here. Probably just redirect to error page
+  console.log('%c ' + response, 'color:red');
+}
